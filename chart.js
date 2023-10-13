a38715330d2c89fad6e9245647b66ef321e267e0
Hammad Ali <ch.hammad2914@gmail.com>
2023-10-13 17:17:40 +0500

    new chart

--- chart.js
+++ chart.js
@@ -8,359 +8,581 @@ var highlightColor = '#5C6BC0',
   
 var points = [ 
   { 
-    x: 'Eliott Nieves', 
-    id: 'president', 
-    attributes: { 
-      role: 'CEO', 
-      photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
-    } 
-  }, 
-  { 
-    x: 'Eliott Nieves', 
-    id: 'president2', 
+    x: 'Kaschif R. Israr', 
+    id: 'Country Head', 
     attributes: { 
       role: 'Country Head', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/1.png'
+      ),
+      email:'Kaschif@eurocentra.com.pk' 
+    } 
+  }, 
+  { 
+    x: 'Annika&nbsp;Schwaegerl', 
+    id: 'SD Euro', 
+    attributes: { 
+      role: 'Sales&nbsp;Director&nbsp;Europe', 
+      photo: getImgText( 
+        './assets/Color/0.png'
+      ),
+      email:'annika_schwaegerl@eurocentra.com.hk' 
     } 
   },
   { 
     x: 'Business Team', 
     id: 'Business', 
-    parent: 'president', 
+    parent: 'Country Head', 
     attributes: { 
       role: '', 
-      photo: '',
+      photo: getImgText( 
+        './assets/team.png'
+      ),
       email: '',
     } 
   }, 
   { 
     x: 'Technical Team', 
     id: 'Technical', 
-    parent: 'president', 
+    parent: 'Country Head', 
     attributes: { 
       role: '', 
-      photo: '',
+      photo: getImgText( 
+        './assets/team.png'
+      ),
       email: ''
     } 
   }, 
   { 
     x: 'Services Team', 
     id: 'Services', 
-    parent: 'president', 
+    parent: 'Country Head', 
     attributes: { 
       role: '', 
-      photo: '',
+      photo: getImgText( 
+        './assets/team.png'
+      ),
       email: ''
     } 
   }, 
   { 
     x: 'Supply Chain', 
     id: 'Supply', 
-    parent: 'president', 
+    parent: 'Country Head', 
     attributes: { 
       role: '', 
-      photo: '',
-      email: ''
-    } 
-  }, 
-  { 
-    x: 'OSG', 
-    id: 'OSG', 
-    parent: 'president', 
-    attributes: { 
-      role: '', 
-      photo: '',
-      email: ''
-    } 
-  }, 
-  { 
-    x: 'David', 
-    id: 'Lead1', 
-    parent: 'Business', 
-    attributes: { 
-      role: 'Business Lead', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/team.png'
+      ),
+      email: ''
     } 
   }, 
   { 
-    x: 'Gethin', 
+    x: 'Operational&nbsp;Support<br/>Group&nbsp;(OSG)', 
+    id: 'OSG', 
+    parent: 'Country Head', 
+    attributes: { 
+      role: '', 
+      photo: getImgText( 
+        './assets/team.png'
+      ),
+      email: ''
+    } 
+  }, 
+  { 
+    x: 'Rana&nbsp;Sohaib&nbsp;Mustafa', 
     id: 'Lead2', 
     parent: 'Technical', 
     attributes: { 
-      role: 'Tech Lead', 
+      role: 'Head&nbsp;of&nbsp;Product<br/>Development&nbsp;&&nbsp;Manufacturing', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/2.png'
+      ),
+      email:'Sohaib@eurocentra.com.pk' 
     } 
   }, 
   { 
-    x: 'Sonnie Kim', 
-    id: 'Lead3', 
+    x: 'Zahid&nbsp;Sajjad', 
+    id: 'Zahid Sajjad', 
     parent: 'Services', 
     attributes: { 
-      role: 'Service Lead', 
+      role: 'Head of Sustainability, <br/> Digital Innovation & Services', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/3.png'
+      ),
+      email:'zahid@eurocentra.com.pk' 
     } 
   }, 
   { 
-    x: 'William', 
+    x: 'Abdul Saboor', 
     id: 'Lead4', 
     parent: 'Supply', 
     attributes: { 
-      role: 'Supply Chain Lead', 
+      role: 'Head of Supply Chain', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/4.png'
+      ),
+      email:'abdul.saboor@eurocentra.com.pk' 
     } 
   }, 
   { 
-    x: 'Chris', 
+    x: 'Madni&nbsp;Khan', 
     id: 'Lead5', 
     parent: 'OSG', 
     attributes: { 
-      role: 'OSG Lead', 
+      role: 'Manager&nbsp;Finance&nbsp;&<br/>Corporate Governance', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/5.png'
+      ),
+      email:'Madni@eurocentra.com.pk' 
     } 
   },
   { 
-    x: 'John', 
+    x: 'Umair&nbsp;Ahmed&nbsp;Siddiqui', 
     id: 'Busemp1', 
-    parent: 'Lead1', 
+    parent: 'Business', 
     attributes: { 
-      role: 'Business employee1', 
+      role: 'Division&nbsp;Head&nbsp;Bonprix&nbsp;-&nbsp;Knits', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/15.png'
+      ),
+      email:'Umair@eurocentra.com.pk' 
     } 
   }, 
   { 
-    x: 'Russel', 
+    x: 'Abdul&nbsp;Hafeez', 
     id: 'Busemp2', 
-    parent: 'Lead1', 
+    parent: 'Business', 
     attributes: { 
-      role: 'Business employee2', 
+      role: 'Division&nbsp;Head&nbsp;Bonprix-Woven', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/17.png'
+      ),
+      email:'abdul.hafeez@eurocentra.com.pk' 
     } 
   }, 
   { 
-    x: 'Messi', 
+    x: 'Muhammad&nbsp;Umar', 
     id: 'Busemp3', 
-    parent: 'Lead1', 
+    parent: 'Business', 
     attributes: { 
-      role: 'Business employee2', 
+      role: 'Division&nbsp;Head&nbsp;-&nbsp;Bonprix / Limango', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/16.png'
+      ),
+      email:'Umer@eurocentra.com.pk' 
     } 
   }, 
   { 
-    x: 'Maxwell', 
+    x: 'Kashif&nbsp;Younus', 
     id: 'Busemp4', 
     parent: 'Busemp1', 
     attributes: { 
-      role: 'Employee', 
+      role: 'Deputy&nbsp;Merchandising&nbsp;Manager', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/21.png'
+      ),
+      email:'Kashif.younus@eurocentra.com.pk' 
     } 
   }, 
   { 
-    x: 'Smith', 
+    x: 'Syed&nbsp;Amir&nbsp;Abbas', 
     id: 'Busemp5', 
     parent: 'Busemp2', 
     attributes: { 
-      role: 'Employee1', 
+      role: 'Deputy&nbsp;Merchandising&nbsp;Manager', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/22.png'
+      ),
+      email:'Aamir.abbas@eurocentra.com.pk' 
     } 
   },
   { 
-    x: 'Marsh', 
-    id: 'Techemp1', 
+    x: 'Product Development', 
+    id: 'PD', 
     parent: 'Lead2', 
     attributes: { 
-      role: 'Tech employee1', 
-      photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+      role: '', 
+      photo: '',
+      email:''
     } 
   }, 
   { 
-    x: 'Alex', 
-    id: 'Techemp2', 
+    x: '3D Product Innovation', 
+    id: '3D', 
     parent: 'Lead2', 
     attributes: { 
-      role: 'Tech employee2', 
-      photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+      role: '', 
+      photo: '',
+      email:''
     } 
   }, 
   { 
-    x: 'Hales', 
-    id: 'Techemp3', 
+    x: 'Quality Development', 
+    id: 'QD', 
     parent: 'Lead2', 
     attributes: { 
-      role: 'Tech employee3', 
-      photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+      role: '', 
+    photo: '',
+      email:''
     } 
   }, 
   { 
-    x: 'Starc', 
-    id: 'Techemp4', 
+    x: 'Textile Testing', 
+    id: 'TT', 
     parent: 'Lead2', 
     attributes: { 
-      role: 'Tech employee4', 
-      photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+      role: '', 
+    photo: '',
+      email:''
     } 
   }, 
   { 
-    x: 'Hazelwood', 
-    id: 'Techemp5', 
+    x: 'WFP Audits', 
+    id: 'WFP', 
     parent: 'Lead2', 
     attributes: { 
-      role: 'Tech employee5', 
-      photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+      role: '', 
+      photo: '',
+      email:''
     } 
   },
   { 
-    x: 'Brock', 
-    id: 'Seremp1', 
-    parent: 'Lead3', 
+    x: 'Khizar Hayat', 
+    id: 'Khizar Hayat', 
+    parent: 'PD', 
     attributes: { 
-      role: 'Service employee1', 
+      role: 'Technical Lead', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
-    } 
-  },   
-  { 
-    x: 'Mark Wood', 
-    id: 'Seremp2', 
-    parent: 'Lead3', 
-    attributes: { 
-      role: 'Service employee2', 
-      photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
-    } 
-  },   
-  { 
-    x: 'Woakes', 
-    id: 'Seremp3', 
-    parent: 'Lead3', 
-    attributes: { 
-      role: 'Service employee3', 
-      photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
-    } 
-  },   
-  { 
-    x: 'Williamson', 
-    id: 'Seremp4', 
-    parent: 'Lead3', 
-    attributes: { 
-      role: 'Service employee4', 
-      photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
-    } 
-  },   
-  { 
-    x: 'Philips', 
-    id: 'Seremp5', 
-    parent: 'Lead3', 
-    attributes: { 
-      role: 'Service employee5', 
-      photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/14.png'
+      ),
+      email:'khizar@eurocentra.com.pk'
     } 
   },
   { 
-    x: 'Conway', 
-    id: 'Osgemp1', 
-    parent: 'Lead5', 
+    x: 'Muhammad Zeeshan', 
+    id: 'Muhammad Zeeshan', 
+    parent: 'Khizar Hayat', 
     attributes: { 
-      role: 'OSG employee1', 
+      role: 'Technical Lead', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/26.png'
+      ),
+      email:'M.zeeshan@eurocentra.com.pk'
     } 
   }, 
   { 
-    x: 'Fregusan', 
-    id: 'Osgemp2', 
-    parent: 'Lead5', 
+    x: 'Muhammad Tahreem', 
+    id: 'Muhammad Tahreem', 
+    parent: 'Muhammad Zeeshan', 
     attributes: { 
-      role: 'OSG employee2', 
+      role: 'Technical Lead', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/23.png'
+      ),
+      email:'tehreem@eurocentra.com.pk'
+    } 
+  },  
+  { 
+    x: 'Imran Amjad', 
+    id: 'Imran Amjad', 
+    parent: '3D', 
+    attributes: { 
+      role: 'Team Lead 3D', 
+      photo: getImgText( 
+        './assets/Color/25.png'
+      ),
+      email:'imran.amjad@eurocentra.com.pk'
     } 
   }, 
   { 
-    x: 'Boult', 
-    id: 'Osgemp3', 
-    parent: 'Lead5', 
+    x: 'MUHAMMAD ASIM', 
+    id: 'MUHAMMAD ASIM', 
+    parent: 'QD', 
     attributes: { 
-      role: 'OSG employee3', 
+      role: 'Regional Quality Head', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/13.png'
+      ),
+      email:'asim@eurocentra.com.pk'
     } 
   }, 
   { 
-    x: 'Styne', 
-    id: 'Osgemp4', 
-    parent: 'Lead5', 
+    x: 'Dania Jamil', 
+    id: 'Dania Jamil', 
+    parent: 'TT', 
     attributes: { 
-      role: 'OSG employee4', 
+      role: 'Testing Manager', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/31.png'
+      ),
+      email:'dania@eurocentra.com.pk'
+    } 
+  },
+  { 
+    x: 'Muhammad Owais', 
+    id: 'Muhammad Owais', 
+    parent: 'Dania Jamil', 
+    attributes: { 
+      role: 'Laboratory Incharge', 
+      photo: getImgText( 
+        './assets/Color/30.png'
+      ),
+      email:''
+    } 
+  },  
+  { 
+    x: 'Ismail Khan', 
+    id: 'Ismail Khan', 
+    parent: 'WFP', 
+    attributes: { 
+      role: 'Manager, Chemical<br/>Management', 
+      photo: getImgText( 
+        './assets/Color/27.png'
+      ),
+      email:'ismail@eurocentra.com.pk'
+    } 
+  },
+  { 
+    x: 'Digitalization Asset<br/>Management (DAM)', 
+    id: 'DAM', 
+    parent: 'Zahid Sajjad', 
+    attributes: { 
+      role: '', 
+      photo: '',
+      email:''
     } 
   }, 
   { 
-    x: 'George', 
-    id: 'Osgemp5', 
-    parent: 'Lead5', 
+    x: 'Sustainability & CSR', 
+    id: 'CSR', 
+    parent: 'Zahid Sajjad', 
     attributes: { 
-      role: 'OSG employee5', 
-      photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+      role: '', 
+      photo: '',
+      email:''
     } 
   }, 
   { 
-    x: 'Crist', 
-    id: 'Osgemp6', 
+    x: 'Logistics', 
+    id: 'Logistics', 
+    parent: 'Zahid Sajjad', 
+    attributes: { 
+      role: '', 
+      photo: '',
+      email:''
+    } 
+  }, 
+  { 
+    x: 'Product Library', 
+    id: 'PL', 
+    parent: 'Zahid Sajjad', 
+    attributes: { 
+      role: '', 
+      photo: '',
+      email:''
+    } 
+  }, 
+  { 
+    x: 'Human Asset<br/>Management (HAM)', 
+    id: 'HAM', 
+    parent: 'Zahid Sajjad', 
+    attributes: { 
+      role: '', 
+      photo: '',
+      email:''
+    } 
+  },
+  { 
+    x: 'Muhammad Aamir', 
+    id: 'Muhammad Aamir', 
+    parent: 'DAM', 
+    attributes: { 
+      role: 'MIS Manager', 
+      photo: getImgText( 
+        './assets/Color/9.png'
+      ), 
+      email:'aamir@eurocentra.com.pk'
+    } 
+  },   
+  { 
+    x: 'Sadad Ali', 
+    id: 'Sadad Ali', 
+    parent: 'Muhammad Aamir', 
+    attributes: { 
+      role: 'MIS Executive', 
+      photo: getImgText( 
+        './assets/Color/19.png'
+      ),
+      email:'sadad@eurocentra.com.pk' 
+    } 
+  },   
+  { 
+    x: 'Muhammad Noman', 
+    id: 'Muhammad Noman', 
+    parent: 'Sadad Ali', 
+    attributes: { 
+      role: 'Excellence Manager', 
+      photo: getImgText( 
+        './assets/Color/8.png'
+      ),
+      email:'Noman@eurocentra.com.pk' 
+    } 
+  },   
+  { 
+    x: 'Ahmed Adeel', 
+    id: 'Ahmed Adeel', 
+    parent: 'CSR', 
+    attributes: { 
+      role: 'Sustainability Manager', 
+      photo: getImgText( 
+        './assets/Color/11.png'
+      ),
+      email:'ahmed.adeel@eurocentra.com.pk'
+    } 
+  },   
+  { 
+    x: 'Basit Ali', 
+    id: 'Basit Ali', 
+    parent: 'Ahmed Adeel', 
+    attributes: { 
+      role: 'CSR Executive', 
+      photo: getImgText( 
+        './assets/Color/18.png'
+      ),
+      email:'Basit.ali@eurocentra.com.pk' 
+    } 
+  },
+  { 
+    x: 'Jameel Ahmed', 
+    id: 'Jameel Ahmed', 
+    parent: 'Basit Ali', 
+    attributes: { 
+      role: 'Project Manager', 
+      photo: getImgText( 
+        './assets/Color/10.png'
+      ),
+      email:'Jameel@eurocentra.com.pk' 
+    } 
+  }, 
+  { 
+    x: 'Grephen Almas', 
+    id: 'Grephen Almas', 
+    parent: 'Logistics', 
+    attributes: { 
+      role: 'Logistics Manager', 
+      photo: getImgText( 
+        './assets/Color/12.png'
+      ),
+      email:'grephen@eurocentra.com.pk' 
+    } 
+  }, 
+  { 
+    x: 'Mehreen Idrees', 
+    id: 'Mehreen Idrees', 
+    parent: 'Grephen Almas', 
+    attributes: { 
+      role: 'Assisstant Logistics Manager', 
+      photo: getImgText( 
+        './assets/Color/20.png'
+      ),
+      email:'Mehreen@eurocentra.com.pk' 
+    } 
+  }, 
+  { 
+    x: 'Sikandar Solangi', 
+    id: 'Sikandar Solangi', 
+    parent: 'PL', 
+    attributes: { 
+      role: 'Librarian', 
+      photo: getImgText( 
+        './assets/Color/00.png'
+      ),
+      email:'' 
+    } 
+  }, 
+  { 
+    x: 'TBH', 
+    id: 'TBH', 
+    parent: 'HAM', 
+    attributes: { 
+      role: 'Talent Acquisition Specialist', 
+      photo: getImgText( 
+        './assets/Color/00.png'
+      ),
+      email:'' 
+    } 
+  }, 
+  { 
+    x: 'Muhammad Safwan Khan', 
+    id: 'Muhammad Safwan Khan', 
+    parent: 'TBH', 
+    attributes: { 
+      role: 'Working Student', 
+      photo: getImgText( 
+        './assets/Color/6.png'
+      ),
+      email:'hr@eurocentra.com.pk' 
+    } 
+  },
+  { 
+    x: 'Sajjad Hussain', 
+    id: 'Sajjad Hussain', 
     parent: 'Lead5', 
     attributes: { 
-      role: 'OSG employee6', 
+      role: 'IT & Communication Manager', 
       photo: getImgText( 
-        'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png'
-      ) 
+        './assets/Color/56.png'
+      ),
+      email:'it@eurocentra.com.pk' 
+    } 
+  },
+  { 
+    x: 'Abdul Manan', 
+    id: 'Abdul Manan', 
+    parent: 'Sajjad Hussain', 
+    attributes: { 
+      role: 'Facility Services Manager', 
+      photo: getImgText( 
+        './assets/Color/57.png'
+      ),
+      email:'abdulmanan@eurocentra.com.pk' 
+    } 
+  },
+  { 
+    x: 'Pervez Ahmed', 
+    id: 'Pervez Ahmed', 
+    parent: 'Abdul Manan', 
+    attributes: { 
+      role: 'Front Desk officer', 
+      photo: getImgText( 
+        './assets/Color/55.png'
+      ),
+      email:'pervez@eurocentra.com.pk' 
+    } 
+  },
+  { 
+    x: 'Naeem', 
+    id: 'Naeem', 
+    parent: 'Pervez Ahmed', 
+    attributes: { 
+      role: 'Facility Officer', 
+      photo: getImgText( 
+        './assets/Color/54.png'
+      ),
+      email:'' 
+    } 
+  },
+  { 
+    x: 'Javaid', 
+    id: 'Javaid', 
+    parent: 'Naeem', 
+    attributes: { 
+      role: 'Chauffeur', 
+      photo: getImgText( 
+        './assets/Color/53.png'
+      ),
+      email:'' 
     } 
   },    
 
@@ -377,6 +599,13 @@ var chart = JSC.chart('chartDiv1', {
     padding: [5, 10], 
     margin: 6 
   }, 
+  annotations: [ 
+    { 
+      position: 'bottom', 
+      label_text: 
+        'Powered by Interactive Technologies Gateway.'
+    } 
+  ],
   
   defaultSeries: { 
     color: normalFill, 
@@ -391,7 +620,7 @@ var chart = JSC.chart('chartDiv1', {
     }, 
     label: { 
       text: 
-        '%photo%name<br><span style="color:#9E9E9E">%role <br/> %email</span>', 
+        '%photo <b>%name</b><br><span style="color:#9E9E9E">%role</span><br/> %email', 
       style_color: 'black'
     }, 
     outline: { color: '#e0e0e0', width: 1 }, 
@@ -500,7 +729,7 @@ function resetStyles(chart) {
   
 function getImgText(name) { 
   return ( 
-    '<img width=50 height=50 align=center margin_bottom=4 margin_top=4 src=' + 
+    '<img width=80 height=80 align=center margin_bottom=4 margin_top=4 src=' + 
     name + 
     '><br>'
   ); 
